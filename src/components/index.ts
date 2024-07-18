2e53de3a34147a95e1cc20fe1c0fe8bccd38f3c6
Исупов Евгений Валерьевич <workline521@gmail.com>
2024-07-18 18:57:38 +0400

    blocks

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 import 'src/components/header'
 import 'src/components/footer'
+import 'src/components/home'
+import 'src/components/product'
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'restyle.css'
 import 'restyle.css/dist/checkboxes.css'
 import 'src/styles/main.scss'
 import 'src/components'
+/* import Swiper from 'swiper'
+import { Navigation, Pagination, Thumbs } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/thumbs' */
 import { modalsInstance } from 'src/scripts/modals-instance'
 import { globalScrollController } from 'src/scripts/global-scroll-controller'
 
@@ -9,3 +17,5 @@ document.addEventListener('DOMContentLoaded', () => {
   modalsInstance.init()
   globalScrollController.listen()
 })
+
+
