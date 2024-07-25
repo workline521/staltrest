29e2edf65d29040410e85eda15426dd6b62503c7
Исупов Евгений Валерьевич <workline521@gmail.com>
2024-07-25 09:42:40 +0400

    picture

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,11 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-const swiper = new Swiper('.info__slider', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true, 
-  },
-}) 
+
 /* popup */
 const popup = document.querySelector('.popup')
 const popupOverlay = document.querySelector('.popup__overlay')
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,27 @@ import 'restyle.css'
 import 'restyle.css/dist/checkboxes.css'
 import 'src/styles/main.scss'
 import 'src/components'
+import Swiper from 'swiper/bundle';
+const swiper = new Swiper('.info__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, 
+  },
+}) 
 
-
-
+const swiperThumbs = new Swiper('.cert__thumbs', {
+  loop: true,
+  spaceBetween: 4,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+})
+const swiperMain = new Swiper('.cert__swiper', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: { swiper: swiperThumbs, },
+}) 
